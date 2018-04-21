abc3eff4f52d68c23ff2fe8364cbf0f5e56ca342
Pedro Ernesto Perdoná Rech <p.perdona@gmail.com>
2018-04-20 22:19:13 -0300

    Adicionando consulta de lista de eventos

--- src/main/java/com/pedroperdona/eventosapp/controllers/EventoController.java
+++ src/main/java/com/pedroperdona/eventosapp/controllers/EventoController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.pedroperdona.eventosapp.models.Evento;
 import com.pedroperdona.eventosapp.repository.EventoRepository;
@@ -18,13 +19,22 @@ public class EventoController {
 	public String formulario() {
 		return "evento/formularioEvento";
 	}
-	
+
 	@RequestMapping(value = "/cadastrarEvento", method = RequestMethod.POST)
 	public String formulario(Evento evento) {
-		
 		repository.save(evento);
-		
 		return "redirect:/cadastrarEvento";
+	}
+
+	@RequestMapping(value = "/eventos")
+	public ModelAndView listaEventos() {
+
+		ModelAndView modelAndView = new ModelAndView("index");
+
+		Iterable<Evento> eventos = repository.findAll();
+		modelAndView.addObject("eventos", eventos);
+
+		return modelAndView;
 	}
 
 }
