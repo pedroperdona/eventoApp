c6cde2d13b5ffaeb66269b80f6496d44eee1366b
Pedro Ernesto Perdoná Rech <p.perdona@gmail.com>
2018-04-23 20:09:40 -0300

    adicionando form convidado

--- src/main/java/com/pedroperdona/eventosapp/controllers/EventoController.java
+++ src/main/java/com/pedroperdona/eventosapp/controllers/EventoController.java
@@ -7,14 +7,19 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.pedroperdona.eventosapp.models.Convidado;
 import com.pedroperdona.eventosapp.models.Evento;
+import com.pedroperdona.eventosapp.repository.ConvidadoRepository;
 import com.pedroperdona.eventosapp.repository.EventoRepository;
 
 @Controller
 public class EventoController {
 
 	@Autowired
-	private EventoRepository repository;
+	private EventoRepository eventoRepository;
+	
+	@Autowired
+	private ConvidadoRepository convidadoRepository;
 
 	@RequestMapping(value = "/cadastrarEvento", method = RequestMethod.GET)
 	public String formulario() {
@@ -23,27 +28,39 @@ public class EventoController {
 
 	@RequestMapping(value = "/cadastrarEvento", method = RequestMethod.POST)
 	public String formulario(Evento evento) {
-		repository.save(evento);
+		eventoRepository.save(evento);
 		return "redirect:/cadastrarEvento";
 	}
 
 	@RequestMapping("/eventos")
-	public ModelAndView listaEventos() {
-
+	public ModelAndView listEventos() {
+		
+		Iterable<Evento> eventos = eventoRepository.findAll();
+		
 		ModelAndView modelAndView = new ModelAndView("index");
-
-		Iterable<Evento> eventos = repository.findAll();
 		modelAndView.addObject("eventos", eventos);
-
 		return modelAndView;
 	}
-	
-	@RequestMapping("/{codigo}")
-	public ModelAndView detalhesEvento(@PathVariable("codigo") Integer codigo) {
-		Evento evento = repository.findByCodigo(codigo);
+
+	@RequestMapping(value = "/{codigo}", method = RequestMethod.GET)
+	public ModelAndView listDetalhesEvento(@PathVariable("codigo") Integer codigo) {
+		
+		Evento evento = eventoRepository.findByCodigo(codigo);
+		
 		ModelAndView modelAndView = new ModelAndView("evento/detalhesEvento");
 		modelAndView.addObject("evento", evento);
 		return modelAndView;
+	}
+	
+	@RequestMapping(value = "/{codigo}", method = RequestMethod.POST)
+	public String saveDetalhesEvento(@PathVariable("codigo") Integer codigo, Convidado convidado) {
+		
+		Evento evento = eventoRepository.findByCodigo(codigo);
+		convidado.setEvento(evento);
+		
+		convidadoRepository.save(convidado);
+		
+		return "redirect:/{codigo}";
 	}
 
 }
---
+++ src/main/java/com/pedroperdona/eventosapp/repository/ConvidadoRepository.java
@@ -0,0 +1,9 @@
+package com.pedroperdona.eventosapp.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.pedroperdona.eventosapp.models.Convidado;
+
+public interface ConvidadoRepository extends CrudRepository<Convidado, String>{
+
+}
